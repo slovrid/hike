a41819f5872489f8e2a7316496ec63f28498ebd7
Sondre Larsen Ovrid <sondre.ovrid@bekk.no>
2024-09-23 17:42:17 +0200

    Prepare for GH-pages

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { page } from '$app/stores';
+    import {base} from "$app/paths"
 	import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
 </script>
@@ -16,11 +17,11 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
+			<li aria-current={$page.url.pathname === `${base}/` ? 'page' : undefined}>
+				<a href="{base}/">Home</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+			<li aria-current={$page.url.pathname === `${base}/about` ? 'page' : undefined}>
+				<a href="{base}/about">About</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
